9a5ff3478c1fb01f8baf201b4260fc4a1a79ee9f
yaseen <yaseenkhan@citiustech.com>
2023-06-19 19:47:46 +0530

    ready for production

--- src/app/adminservice.service.ts
+++ src/app/adminservice.service.ts
@@ -1,35 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminserviceService {
   flightdata:any;
+  //url:String = "http://localhost:8081/"
   constructor(private http:HttpClient) { }
 
   addFlight(input:any){
-    return this.http.post("http://localhost:8082/flightregister",input);
+    return this.http.post(`${environment.apiUrl}/flightregister`,input);
 
   }
 
   getallFlight(){
-    return this.http.get("http://localhost:8082/getallflight")
+    return this.http.get(`${environment.apiUrl}/getallflight`)
   }
 
   getallCustomer(){
-    return this.http.get("http://localhost:8082/users/getallcustomer")
+    return this.http.get(`${environment.apiUrl}/users/getallcustomer`)
   }
 
   getallBooking(){
-    return this.http.get("http://localhost:8082/getallbooking")
+    return this.http.get(`${environment.apiUrl}/getallbooking`)
   }
 
   updateFlight(input:any,id:number){
     const flightid=id
-    return this.http.put("http://localhost:8082/flightupdate/"+id,input)
+    return this.http.put(`${environment.apiUrl}/flightupdate/`+id,input)
   }
 
   deleteFlight(id:number){
-    return this.http.delete("http://localhost:8082/flightdelete/"+id)
+    return this.http.delete(`${environment.apiUrl}/flightdelete/`+id)
   }
 }
---
+++ src/assets/env.template.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+    window['env'] = window['env'] || {};
+
+ 
+
+   
+
+ 
+
+    // Environment variables
+
+    window['env']['apiUrl'] = '${API_URL}';
+
+    window['env']['aspnetcoreEnvironment'] = '${ASPNETCORE_ENVIRONMENT}';
+
+  })(this);
